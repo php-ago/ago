c42301ccd4fd8b40a23a6eed3a664cad8faab4bc
SerhiiCho <serhiicho@protonmail.com>
2024-12-05 09:47:10 +0200

    Add comments to config

--- src/Config.php
+++ src/Config.php
@@ -6,7 +6,17 @@ namespace Serhii\Ago;
 
 class Config
 {
-    public function __construct(public readonly Lang $lang = Lang::EN)
-    {
+    /**
+     * @param Lang|null $lang Language code following ISO 639-1
+     * standard. Default is English.
+     *
+     * @param LangSet|null $customTranslations Custom translations
+     * will override default translations with the same keys. Use
+     * it to add or change translations. Default is null.
+     */
+    public function __construct(
+        public readonly Lang|null $lang = Lang::EN,
+        public readonly LangSet|null $customTranslations = null,
+    ) {
     }
 }
